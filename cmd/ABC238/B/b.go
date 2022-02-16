057ed96233e4736cd660ace9a6d3db22d9af4754
ryochang122 <pia.no.jac.blue.moon1995@gmail.com>
2022-02-16 22:53:48 +0900

    solved probrem b

--- cmd/ABC238/B/b.go
+++ cmd/ABC238/B/b.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -14,17 +16,34 @@ func nextLine() string{
   return sc.Text()
 }
 
+func atoi(s string) int {
+  var i int
+  i,_=strconv.Atoi(s)
+  return i
+}
+
 func main(){
 
   var n int
   var cnt int
+  var max int
   var sl []int
+
   fmt.Scan(&n)
   inputs := strings.Split(nextLine()," ")
+
   sl = append(sl, 0,360)
   for _,nm := range inputs {
-    cnt += nm
-    sl = append(sl, cnt%360)
+    cnt += atoi(nm)
+    cnt %= 360
+    sl = append(sl, cnt)
   }
 
+  sort.Slice(sl, func(i,j int) bool{return sl[i] > sl[j]})
+  for i:=0;i<len(sl)-1;i++{
+    if d := sl[i] - sl[i+1];max<d{
+      max = d
+    }
+  }
+  fmt.Println(max)
 }
